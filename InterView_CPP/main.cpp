0d63a64a9254bee754f1d67dd1bce5ce28f5d87d
konglee <konglee873@gmail.com>
2017-06-02 09:29:56 +0800

    Initial Commit

---
+++ InterView_CPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InterView_CPP
+//
+//  Created by kong on 17/6/2.
+//  Copyright © 2017年 konglee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
